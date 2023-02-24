518e9a330169f1646f6cc41bc62d1bc221ea660c
SeverinoRibNeto <severino.neto@dcx.ufpb.br>
2023-02-24 15:40:26 -0300

    Criação da urls.py de "usuarios"

---
+++ usuarios/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("cadastro/", views.cadastro_usuario, name="cadastro_usuario"),
+    path("login/", views.login_usuario, name="login_usuario")
+]
